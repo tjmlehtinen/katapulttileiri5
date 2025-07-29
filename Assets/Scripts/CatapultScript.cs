36bdee095dc6e5bca7952e2e059e440d03e7503c
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2025-07-29 15:23:38 +0300

    toka päivä

--- Assets/Scripts/CatapultScript.cs
+++ Assets/Scripts/CatapultScript.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class CatapultScript : MonoBehaviour
 {
+    public Transform shootPoint;
+    public Rigidbody ammoBody;
+    public Vector3 launchDirection = new Vector3(0, 1, 1);
+    public float launchForce = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("skripti toimii");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Launch();
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetCatapult();
+        }
+    }
+
+    void Launch()
+    {
+        ammoBody.isKinematic = false;
+        ammoBody.AddForce(launchDirection.normalized * launchForce, ForceMode.Impulse);
+    }
+
+    void ResetCatapult()
+    {
+        ammoBody.isKinematic = true;
+        ammoBody.transform.position = shootPoint.position;
     }
 }
