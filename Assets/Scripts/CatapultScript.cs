154decb6cd871155ab1895484a59d7f2d3b8d233
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2025-07-30 15:15:48 +0300

    kolmas päivä

--- Assets/Scripts/CatapultScript.cs
+++ Assets/Scripts/CatapultScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class CatapultScript : MonoBehaviour
 {
+    public Transform turret;
     public Transform shootPoint;
     public Rigidbody ammoBody;
     public Vector3 launchDirection = new Vector3(0, 1, 1);
     public float launchForce = 10.0f;
+    public float rotationSpeed = 100.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,17 +25,26 @@ public class CatapultScript : MonoBehaviour
         {
             ResetCatapult();
         }
+        RotateCatapult();
     }
 
     void Launch()
     {
+        ammoBody.transform.parent = null;
         ammoBody.isKinematic = false;
         ammoBody.AddForce(launchDirection.normalized * launchForce, ForceMode.Impulse);
     }
 
     void ResetCatapult()
     {
+        ammoBody.transform.parent = shootPoint;
         ammoBody.isKinematic = true;
         ammoBody.transform.position = shootPoint.position;
+    }
+
+    void RotateCatapult()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        turret.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
